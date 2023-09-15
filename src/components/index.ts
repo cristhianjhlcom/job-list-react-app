7cf112285bb5b05ad9e57872c4484778a4e97a84
cristhianjhlcom <cristhianjhl.web@gmail.com>
2023-09-15 15:41:25 -0500

    add global styles

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export { default as Badge } from './Badge';
+export { default as Banner } from './Banner';
+export { default as Filters } from './Filters';
+export { default as JobList } from './JobList';
+export { default as Tag } from './Tag';
