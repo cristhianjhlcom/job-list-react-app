286c9d6025886037fbc6048d69117f579825ba7c
cristhianjhlcom <cristhianjhl.web@gmail.com>
2023-09-15 22:55:34 -0500

    add job types

--- src/type/Job.d.ts
+++ src/type/Job.d.ts
@@ -12,4 +12,23 @@ export type Job = {
 	postedAt: string;
 	role: string;
 	tools: string[];
-}
+};
+
+export type JobState = {
+	data: Job[];
+	filteredJobs: Job[];
+	filters: Set<string>;
+	loading: boolean;
+	error: null | string;
+};
+
+export type JobListContextType = {
+	data: Job[];
+	filteredJobs: Job[];
+	filters: Set<string>;
+	loading: boolean;
+	error: null | string;
+	handleClearFilters: () => void;
+	handleAddFilter: (filter: string) => void;
+	handleRemoveFilter: (filter: string) => void;
+};
